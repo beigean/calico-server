223080626654a9f7b299303d1db70953142de2d3
goro9 <sokenbiccha@icloud.com>
2020-03-27 01:04:34 +0900

    create template

--- main.go
+++ main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
-	_ "testapi/routers"
-
-	"github.com/astaxie/beego"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-	}
-	beego.Run()
+	r := gin.Default()
+	r.GET("/ping", sample)
+	r.GET("/todo", todo_get)
+	r.POST("/todo", todo_post)
+	r.PUT("/todo", todo_put)
+	r.DELETE("/todo", todo_delete)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func sample(c *gin.Context) {
+	// log.Printf()
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func todo_get(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "get dayo",
+	})
+}
+
+func todo_post(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "post dayo",
+	})
+}
+
+func todo_put(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "put dayo",
+	})
+}
+
+func todo_delete(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "delete dayo",
+	})
 }
