a401e9a3938a1d9f3689a94ba1656a6f84927e39
goro9 <sokenbiccha@icloud.com>
2020-03-29 17:45:21 +0900

    add tour of go map exercise

---
+++ samples/tog_03/main.go
@@ -0,0 +1,21 @@
+// ref. https://go-tour-jp.appspot.com/moretypes/23
+package main
+
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	m := make(map[string]int)
+	for i := 0; i < len(words); i++ {
+		m[words[i]]++
+	}
+	return m
+}
+
+func main() {
+	wc.Test(WordCount)
+}
