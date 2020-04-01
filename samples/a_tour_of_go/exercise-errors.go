73f391642d37f04550d84a838d2a6187cc089a36
goro9 <sokenbiccha@icloud.com>
2020-04-02 01:29:28 +0900

    add exercise errors

--- samples/a_tour_of_go/exercise-errors.go
+++ samples/a_tour_of_go/exercise-errors.go
@@ -8,19 +8,17 @@ import (
 
 type ErrNegativeSqrt float64
 
-func (e *ErrNegativeSqrt) Error() string {
-	return fmt.Sprintf("cannot Sqrt negative number: -2")
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %.0f", float64(e))
 }
 
 func Sqrt(x float64) (float64, error) {
-	error err = nil
 	if x < 0 {
-		err = ErrNegativeSqrt(-2)
+		return 0, ErrNegativeSqrt(x)
 	}
 
 	z, z_prev := 1.0, x
 	const accuracy = 1.0E-10
-	fmt.Println(accuracy)
 	for i := 0; i < 10; i++ {
 		z -= (z*z - x) / (2 * z)
 		fmt.Println(z)
@@ -29,11 +27,16 @@ func Sqrt(x float64) (float64, error) {
 		}
 		z_prev = z
 	}
-	return z
+	return z, nil
 }
 
 func main() {
-	target := float64(3648)
-	fmt.Println("self", Sqrt(target))
+	target := float64(-3648)
+	if result, err := Sqrt(target); err == nil {
+		fmt.Println("self", result)
+	} else {
+		fmt.Println(err)
+	}
+
 	fmt.Println("math", math.Sqrt(target))
 }
