4c842073bcf9d15529677a2a047fc91fd87f7db1
goro9 <sokenbiccha@icloud.com>
2020-04-03 23:25:26 +0900

    add exercise images

---
+++ samples/a_tour_of_go/exercise-images.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"image"
+	"image/color"
+
+	"golang.org/x/tour/pic"
+)
+
+type Image struct{}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+	w, h := 256, 256
+	return image.Rect(0, 0, w, h)
+}
+
+func (i Image) At(x, y int) color.Color {
+	v := uint8((x + y) / 2)
+	return color.RGBA{v, v, 255, 255}
+}
+
+func main() {
+	m := Image{}
+	pic.ShowImage(m)
+}
